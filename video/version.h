41b51c0dd3361dff507905fdc29f1982d2042ce9
Steve Sims <steve@stevesims.dev>
2023-11-02 13:55:01 +0000

    change version to next patch

--- video/version.h
+++ video/version.h
@@ -3,9 +3,9 @@
 
 #define		VERSION_MAJOR		2
 #define		VERSION_MINOR		0
-#define		VERSION_PATCH		0
-// #define		VERSION_CANDIDATE	0			// Optional
-// #define		VERSION_TYPE		"Release"	// RC, Alpha, Beta, etc.
+#define		VERSION_PATCH		1
+#define		VERSION_CANDIDATE	1			// Optional
+#define		VERSION_TYPE		"Alpha "	// RC, Alpha, Beta, etc.
 
 #define		VERSION_VARIANT		"Console8"
 
