16d8073a83f46eed326ff438a6d2132246ba5f88
Steve Sims <steve@stevesims.dev>
2023-12-19 15:21:46 +0000

    update version to reflect experimental branch

--- video/version.h
+++ video/version.h
@@ -2,10 +2,10 @@
 #define VERSION_H
 
 #define		VERSION_MAJOR		2
-#define		VERSION_MINOR		3
+#define		VERSION_MINOR		4
 #define		VERSION_PATCH		0
-#define		VERSION_CANDIDATE	0			// Optional
-#define		VERSION_TYPE		"Release"	// RC, Alpha, Beta, etc.
+#define		VERSION_CANDIDATE	1			// Optional
+#define		VERSION_TYPE		"Experimental "	// RC, Alpha, Beta, etc.
 
 #define		VERSION_VARIANT		"Console8"
 
