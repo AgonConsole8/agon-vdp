3f33eec6d7ed327f2038d4dbcdb8420ce249b1aa
Steve Sims <steve@stevesims.dev>
2024-01-21 17:14:01 +0000

    bump version to 2.5.0

--- video/version.h
+++ video/version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 #define		VERSION_MAJOR		2
-#define		VERSION_MINOR		4
+#define		VERSION_MINOR		5
 #define		VERSION_PATCH		0
 #define		VERSION_CANDIDATE	0			// Optional
 #define		VERSION_TYPE		"Release"	// RC, Alpha, Beta, etc.
