dacb520e5c144dc8289331d3b0cc2717152c3365
Curtis Whitley <cwhitley@pesa.com>
2024-04-22 14:22:07 -0400

    Add initial pingo files

---
+++ video/pingo/render/texture.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "pixel.h"
+#include "renderable.h"
+#include "../math/vec2.h"
+
+typedef struct  Texture {
+   Vec2i size;
+   Pixel * frameBuffer;
+} Texture;
+
+extern int texture_init( Texture * f, Vec2i size, Pixel *);
+
+extern Renderable texture_as_renderable( Texture * s);
+
+extern void  texture_draw(Texture * f, Vec2i pos, Pixel color);
+
+extern Pixel texture_read(Texture * f, Vec2i pos);
+
+extern Pixel texture_readF(Texture * f, Vec2f pos);
+
