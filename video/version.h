0075d3d099eea4e508026788c00f59ec1f0d7280
Steve Sims <steve@stevesims.dev>
2024-08-24 12:47:11 +0100

    bump version to 2.10.0

--- video/version.h
+++ video/version.h
@@ -2,8 +2,8 @@
 #define VERSION_H
 
 #define		VERSION_MAJOR		2
-#define		VERSION_MINOR		9
-#define		VERSION_PATCH		2
+#define		VERSION_MINOR		10
+#define		VERSION_PATCH		0
 #define		VERSION_CANDIDATE	0			// Optional
 #define		VERSION_TYPE		"Release"	// RC, Alpha, Beta, etc.
 
