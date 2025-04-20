4b67980a4f388088523ef4333f7e6ea158bb2833
Steve Sims <steve@stevesims.dev>
2025-04-20 17:52:19 +0100

    bump version to 2.14.0
    the `VERSION_VARIANT` is now “Platform” to be in-line with the new MOS 3.0 release
    
    support for a `VERSION_SUBTITLE` added, which for this release is “Dressing Gown”, after all, can one imagine Arthur without his dressing gown?

--- video/version.h
+++ video/version.h
@@ -2,11 +2,12 @@
 #define VERSION_H
 
 #define		VERSION_MAJOR		2
-#define		VERSION_MINOR		13
-#define		VERSION_PATCH		1
+#define		VERSION_MINOR		14
+#define		VERSION_PATCH		0
 #define		VERSION_CANDIDATE	0			// Optional
 #define		VERSION_TYPE		"Release"	// RC, Alpha, Beta, etc.
 
-#define		VERSION_VARIANT		"Console8"
+#define		VERSION_VARIANT		"Platform"
+#define     VERSION_SUBTITLE    "Dressing Gown"
 
 #endif // VERSION_H
